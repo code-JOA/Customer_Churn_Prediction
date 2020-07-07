d0c8c2731566a3fb545919002f32a85f68234852
code-JOA <joshbillion1@gmail.com>
2020-07-07 22:41:22 +0100

    work in progress

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Note : This Project is still under construction. Thank you for your patience. Will be due by this weekend 9/07/2020.
+
 # Customer_Churn_Prediction
 
 # Author : Joshua Owusu Ankomah
--- app.py
+++ app.py
@@ -19,9 +19,10 @@ def index():
 
 @app.route("/predict", methods=["POST"])
 def predict():
+    
 
 
 
 
 if __name__ =="__main__":
-    app.run(host="0.0.0.0" , port=5000)    
+    app.run(host="0.0.0.0" , port=5000)
